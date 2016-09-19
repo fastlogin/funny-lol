f6d9eb54b492c0fba099ab0d08ce15f56869a8ec
fastlogin <gd264@cornell.edu>
2016-09-19 03:46:32 -0400

    WOO sequel pro my old friend

--- api-server/app/models/match.rb
+++ api-server/app/models/match.rb
@@ -29,7 +29,22 @@ class Match < ApplicationRecord
 		Match.exists?(match_json["matchId"])
 	end
 
-	# Create a match from a match JSON and save it to database.
+	# Create a match from a match JSON and save it to database and return it.
 	def self.create_match(match_json)
+		@match = Match.new(:match_id => match_json["matchId"])
+		@match.map_id = match_json["mapId"]
+    	@match.region = match_json["region"]
+    	# convert long to DateTime,(time since epoch milliseconds / 1000 = time since epoch seconds)
+    	@match.match_date_time = Time.at(match_json["matchCreation"]/1000)
+    	@match.match_duration = match_json["matchDuration"]
+    	@match.match_queue_type = match_json["queueType"]
+    	@match.match_mode = match_json["matchMode"]
+    	@match.match_type = match_json["matchType"]
+    	@match.season = match_json["season"]
+    	@match.platform = match_json["platformId"]
+    	@match.has_been_classified = false
+    	@match.is_funny = false
+    	@match.save
+    	@match
 	end
 end
